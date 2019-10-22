4a0f18521c36a5982a39aca21ad1cb3967cecbe8
AlvaroVV <uo232635@uniovi.es>
2019-10-22 22:29:50 +0200

    Pyramide and Cube

--- Learning/src/Init/Lesson1.cpp
+++ Learning/src/Init/Lesson1.cpp
@@ -83,32 +83,85 @@ int DrawGLScene(GLvoid)
 
 	//Transformamos la matriz MODELVIEW, por lo que alejamos la escena en -z.
 	
-	glTranslatef(-1.5f, 0.0f, -6.0f);
-	glRotatef(rtri, 0.0f, 1.0f, 0.0f);
+	glTranslatef(-1.5f, 0.0f, -6.0f); //Move MODELVIEW left and into screen
+	glRotatef(rtri, 0.0f, 1.0f, 0.0f); //Rotate the pyramid
 	glBegin(GL_TRIANGLES);
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, -1.0f, 0.0f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 0.0f);
+	//FRONT
+		glColor3f(1.0f, 0.0f, 0.0f); //Red
+		glVertex3f(0.0f, 1.0f, 0.0f); //top
+		glColor3f(0.0f, 1.0f, 0.0f); // Green
+		glVertex3f(-1.0f, -1.0f, 1.0f); //Left
+		glColor3f(0.0f, 0.0f, 1.0f); //Blue
+		glVertex3f(1.0f, -1.0f, 1.0f); //Right
+	//RIGHT
+		glColor3f(1.0f, 0.0f, 0.0f); //Red
+		glVertex3f(0.0f, 1.0f, 0.0f); //top
+		glColor3f(0.0f, 0.0f, 1.0f); // Blue
+		glVertex3f(1.0f, -1.0f, 1.0f); //Left
+		glColor3f(0.0f, 1.0f, 0.0f); //Green
+		glVertex3f(1.0f, -1.0f, -1.0f); //Right
+	//BACK
+		glColor3f(1.0f, 0.0f, 0.0f); //Red
+		glVertex3f(0.0f, 1.0f, 0.0f); //top
+		glColor3f(0.0f, 1.0f, 0.0f); //Green
+		glVertex3f(1.0f, -1.0f, -1.0f); //Left
+		glColor3f(0.0f, 0.0f, 1.0f); //Blue
+		glVertex3f(-1.0f, -1.0f, -1.0f); //Right
+	//LEFT
+		glColor3f(1.0f, 0.0f, 0.0f); //Red
+		glVertex3f(0.0f, 1.0f, 0.0f); //top
+		glColor3f(0.0f, 0.0f, 1.0f); //Blue
+		glVertex3f(-1.0f, -1.0f, -1.0f); //Left
+		glColor3f(0.0f, 1.0f, 0.0f); // Green
+		glVertex3f(-1.0f, -1.0f, 1.0f); //Right
 	glEnd();
 
 	glLoadIdentity();                   // Reset The Current Modelview Matrix
-	glTranslatef(1.5f, 0.0f, -6.0f);              // Move Right 1.5 Units And Into The Screen 6.0
-	glRotatef(rquad, 1.0f, 0.0f, 0.0f);
+	glTranslatef(1.5f, 0.0f, -7.0f);              // Move Right 1.5 Units And Into The Screen 6.0
+	glRotatef(rquad, 1.0f, 1.0f, 1.0f); //Rotate the cube in X,Y,Z
 
 	glBegin(GL_QUADS);  
 	// Draw A Quad
-	glColor3f(0.5f, 0.5f, 1.0f);
-	glVertex3f(-1.0f, 1.0f, 0.0f);              // Top Left
-	glVertex3f(1.0f, 1.0f, 0.0f);              // Top Right
-	glVertex3f(1.0f, -1.0f, 0.0f);              // Bottom Right
-	glVertex3f(-1.0f, -1.0f, 0.0f);              // Bottom Left
+	//TOP
+		glColor3f(0.0f, 1.0f, 0.0f); //Green
+		glVertex3f(1.0f, 1.0f, -1.0f);// Top Right
+		glVertex3f(-1.0f, 1.0f, -1.0f);// Top Left
+		glVertex3f(-1.0f, 1.0f, 1.0f);// Bottom Left
+		glVertex3f(1.0f, 1.0f, 1.0f);// Bottom RIght
+	//BOT
+		glColor3f(1.0f, 0.5f, 0.0f); //Orange
+		glVertex3f(1.0f, -1.0f, -1.0f);// Top Right
+		glVertex3f(-1.0f, -1.0f, -1.0f);// Top Left
+		glVertex3f(-1.0f, -1.0f, 1.0f);// Bottom Left
+		glVertex3f(1.0f, -1.0f, 1.0f);// Bottom Right
+	//FRONT
+		glColor3f(1.0f, 0.0f, 0.0f); //Red
+		glVertex3f(1.0f, 1.0f, 1.0f);// Top RIght
+		glVertex3f(-1.0f, 1.0f, 1.0f);// Top Left
+		glVertex3f(-1.0f, -1.0f, 1.0f);// Bottom Left
+		glVertex3f(1.0f, -1.0f, 1.0f);// Bottom Right
+	//BACK
+		glColor3f(1.0f, 1.0f, 0.0f); //Yellow
+		glVertex3f(1.0f, 1.0f, -1.0f);// Top Right
+		glVertex3f(-1.0f, 1.0f, -1.0f);// Top Left
+		glVertex3f(-1.0f, -1.0f, -1.0f);// Bottom Left
+		glVertex3f(1.0f, -1.0f, -1.0f);// Bottom Right
+	//RIGHT
+		glColor3f(0.0f, 0.0f, 1.0f); //Blue
+		glVertex3f(1.0f, 1.0f, -1.0f);// Top Left
+		glVertex3f(1.0f, 1.0f, 1.0f);// Top RIght
+		glVertex3f(1.0f, -1.0f, 1.0f);// Bottom Right
+		glVertex3f(1.0f, -1.0f, -1.0f);// Bottom Left
+	//LEFT
+		glColor3f(1.0f, 0.0f, 1.0f); //Violet
+		glVertex3f(-1.0f, 1.0f, -1.0f);// Top Left
+		glVertex3f(-1.0f, 1.0f, 1.0f);// Top RIght
+		glVertex3f(-1.0f, -1.0f, 1.0f);// Bottom Right
+		glVertex3f(-1.0f, -1.0f, -1.0f);// Bottom Left
 	glEnd();                            // Done Drawing The Quad
 	
-	rtri += 0.2f;
-	rquad -= 0.15f;
+	rtri += 0.5f;
+	rquad -= 0.5f;
 	return TRUE;
 }
 
