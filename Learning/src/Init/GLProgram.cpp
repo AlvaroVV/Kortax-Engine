26c08922df7440c1255abc84fad8cd12301ba21d
Alvevi <alvaro.velvi@gmail.com>
2020-12-12 18:36:35 +0100

    Actualizado el GLPRogram para lectura de texturas
    Voy necesitando mover funcionalidades. Una clase TextureSample o algo por el estilo.

--- Learning/src/Init/GLProgram.cpp
+++ Learning/src/Init/GLProgram.cpp
@@ -1,6 +1,8 @@
 #include "GLProgram.h"
 
 #include "..\utils\PngResolver.h"
+#include "..\utils\BmpResolver.h"
+#include "..\utils\TargaResolver.h"
 
 
 
@@ -54,7 +56,147 @@ GLuint GLProgram::_loadPNGTexture(const char* fileName)
 	return tex;
 }
 
-void GLProgram::processInput(bool* keys)
+/*
+* TODO: Crear una Facade/Factory para enmascarar el tema de manejar diferentes archivos. 
+*
+*/
+GLuint GLProgram::_loadTGATexture(const char* fileName)
+{
+	GLuint tex;
+	TGA::TGAFILE tgaFile;
+
+	TGA::LoadTGAFile(fileName, &tgaFile);
+	glGenTextures(1, &tex);
+	//Bindeamos el texture object
+	glBindTexture(GL_TEXTURE_2D, tex);
+
+	int align;
+
+	if (!(tgaFile.imageWidth & 3)) {
+		align = 4;
+	}
+	else if (!tgaFile.imageWidth & 1) {
+		align = 2;
+	}
+	else {
+		align = 1;
+	}
+	glPixelStorei(GL_UNPACK_ALIGNMENT, align);
+	//Cargamos la imagen
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tgaFile.imageWidth, tgaFile.imageHeight, 0,
+		GL_RGB, GL_UNSIGNED_BYTE, (GLvoid *)tgaFile.imageData);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	return tex;
+}
+
+GLuint GLProgram::_loadBMPTexture(const char* fileName)
+{
+	GLuint tex;
+	BMP::BITMAPINFOHEADER	bitmapInfoHeader;
+	unsigned char*		bitmapData;
+
+	bitmapData = BMP::LoadBitmapFile(fileName, &bitmapInfoHeader);
+	glGenTextures(1, &tex);
+	//Bindeamos el texture object
+	glBindTexture(GL_TEXTURE_2D, tex);
+
+	int align;
+
+	if (!(bitmapInfoHeader.biWidth & 3)) {
+		align = 4;
+	}
+	else if (!bitmapInfoHeader.biWidth & 1) {
+		align = 2;
+	}
+	else {
+		align = 1;
+	}
+	glPixelStorei(GL_UNPACK_ALIGNMENT, align);
+	//Cargamos la imagen
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight, 0,
+		GL_RGB, GL_UNSIGNED_BYTE, (GLvoid *)bitmapData);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	return tex;
+}
+
+void GLProgram::_saveScreenshot(FILEType type, unsigned char *imageData, int winWidth, int winHeight)
+{
+	imageData = (unsigned char*)malloc(winWidth*winHeight * 3);
+	memset(imageData, 0, winWidth * winHeight * 3);
+
+	glReadPixels(0, 0, winWidth - 1, winHeight - 1, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+
+	//TODO: Mover esto a un filesystem.
+	switch (type)
+	{
+	case BMP:
+		BMP::WriteBitmapFile("writeout.bmp", winWidth, winHeight, (unsigned char*)imageData);
+		break;
+	case TARGA:
+		break;
+	default:
+		break;
+	}
+	
+}
+
+GLvoid GLProgram::_drawCube(float xPos, float yPos, float zPos )
+{
+	glPushMatrix();
+
+	glTranslatef(xPos, yPos, zPos);
+
+	glBegin(GL_QUADS);
+	//top face
+	glNormal3f(0.0f,1.0f,0.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(0.0f, 0.0f, -1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 0.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 0.0f, 0.0f);
+	//front face
+	
+	glNormal3f(0.0f, 0.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, 0.0f, 0.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(0.0f, -1.0f, 0.0f);
+	//right face
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(0.0f, -1.0f, 0.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(0.0f, -1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(0.0f, 0.0f, -1.0f);
+	//left face
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, 0.0f, 0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, 0.0f, -1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+	//bottomface
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f, -1.0f, 0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(0.0f, -1.0f, -1.0f);
+	
+	//back
+	glNormal3f(0.0f, 0.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(0.0f, 0.0f, -1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(0.0f, -1.0f, -1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 0.0f, -1.0f);
+	
+	glEnd();
+	
+	glPopMatrix();
+}
+
+void GLProgram::processInput(bool* keys, int mouseX, int mouseY)
 {
 
 }
--- Learning/src/Init/GLProgram.h
+++ Learning/src/Init/GLProgram.h
@@ -4,16 +4,34 @@
 #include <gl\GLU.h>
 
 
+
 class GLProgram
 {
+protected:
+	enum FILEType
+	{
+		TARGA,
+		BMP,
+		PNG
+	};
+
 public:
 	virtual void init() = 0;
 	virtual void draw() = 0;
 
 	//TODO: Ahora mismo virtual pura no se puede. Crear un método virtual vacío de momento y mantener lo comun (Escape y F1) en el main. Context/Window?
-	virtual void processInput(bool* keys);
+	virtual void processInput(bool* keys, int mouseX, int mouseY);
 
 protected:
+	//TODO: Facade o Factory para quitar esta funcionalidad de aqui
+	//Al final se necesita unsigned char* data, los tamaños y el bitCount.
+	//Crear TextureSample class
 	GLuint _loadPNGTexture(const char* fileName);
+	GLuint _loadBMPTexture(const char* fileName);
+	GLuint _loadTGATexture(const char* fileName);
+
+	void _saveScreenshot(FILEType type, unsigned char *imageData, int winWidth, int winHeight);
+
+	GLvoid _drawCube(float xPos=0.0f, float yPos = 0.0f, float zPos = 0.0f);
 };
 
